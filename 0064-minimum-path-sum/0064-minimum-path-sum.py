b0813e6d3c950030ee04e33dee0cddfb2c217db7
Ariyan181005 <139477779+Ariyan181005@users.noreply.github.com>
2026-08-19 21:43:52 +0530

    Time: 15 ms (66.78%), Space: 14.2 MB (15.63%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -4,18 +4,17 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        m = len(grid)
-        n = len(grid[0])
-        memo = {}
-        def solve(i, j):
-            if i >= m or j >= n:
-                return float('inf')
-            if i == m - 1 and j == n - 1:
-                return grid[i][j]
-            if (i, j) in memo:
-                return memo[(i, j)]
-            down = solve(i + 1, j)
-            right = solve(i, j + 1)
-            memo[(i, j)] = grid[i][j] + min(down, right)
-            return memo[(i, j)]
-        return solve(0, 0)
+        m, n = len(grid), len(grid[0])
+        dp = [[i]* n for i in range(m)]
+        dp[0][0] = grid[0][0]
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    dp[0][0] = grid[0][0]
+                elif i == 0:
+                    dp[0][j] = dp[0][j-1] + grid[i][j]
+                elif j == 0:
+                    dp[i][0] = dp[i-1][0] + grid[i][j]
+                else:
+                    dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1])
+        return dp[m-1][n-1]
