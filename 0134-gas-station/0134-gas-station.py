e0dd4dc0709ae05e5fe0b7dcc01092d8dc6efc40
Ariyan181005 <139477779+Ariyan181005@users.noreply.github.com>
2026-08-25 19:26:37 +0530

    Time: 21 ms (81.7%), Space: 18 MB (28.17%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -5,16 +5,13 @@ class Solution(object):
         :type cost: List[int]
         :rtype: int
         """
-        tot=0
-        tank=0
-        st=0
-        for i in range(len(gas)):
-            tank+=gas[i]-cost[i]
-            tot+=gas[i]-cost[i]
-            if tank<0:
-                st=i+1
-                tank=0
-        if tot>=0:
-            return st
-        else:
+        if sum(gas)<sum(cost):
             return -1
+        t=0
+        s=0
+        for i in range(len(gas)):
+            t+=gas[i]-cost[i]
+            if t<0:
+                s=i+1
+                t=0
+        return s
