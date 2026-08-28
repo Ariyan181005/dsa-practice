dad81878cf578632a51d3f18079e26b698ae0b40
Ariyan181005 <139477779+Ariyan181005@users.noreply.github.com>
2026-08-28 23:06:58 +0530

    Time: 920 ms (18.15%), Space: 31.1 MB (38.78%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -4,12 +4,12 @@ class Solution(object):
         :type s: str
         :rtype: List[str]
         """
-        se=set()
+        seen=set()
         ans=set()
         for i in range(len(s)-9):
             x=s[i:i+10]
-            if x in se:
+            if x in seen:
                 ans.add(x)
             else:
-                se.add(x)
+                seen.add(x)
         return list(ans)
