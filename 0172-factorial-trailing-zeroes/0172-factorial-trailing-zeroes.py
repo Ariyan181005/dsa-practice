514cdea6c67c2ebe219caf332bc20534cc1c23ce
Ariyan181005 <139477779+Ariyan181005@users.noreply.github.com>
2026-08-28 22:26:02 +0530

    Time: 5063 ms (5.04%), Space: 23.4 MB (5.04%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def trailingZeroes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        def fact(a):
+            if a==0 or a==1:
+                return 1
+            return a*fact(a-1)
+        f=str(fact(n))
+        c=0
+        for i in range(len(f)-1,-1,-1):
+            if f[i]=="0":
+                c+=1
+            else:
+                break
+        return c
