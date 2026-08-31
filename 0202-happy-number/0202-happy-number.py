278282e8fad3ea95881e3cbcf2bdfc99af0821fc
Ariyan181005 <139477779+Ariyan181005@users.noreply.github.com>
2026-08-31 21:00:04 +0530

    Time: 4 ms (33.46%), Space: 12.3 MB (54.49%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -4,6 +4,7 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
+        """
         s=set()
         while n!=1:
             if n in s:
@@ -16,3 +17,11 @@ class Solution(object):
                 n//=10
             n=su
         return True
+        """
+        while(1):
+            c=0
+            for i in str(n):
+                c+=int(i)**2
+                n=c
+            if(n==4):return False
+            if(n==1):return True
