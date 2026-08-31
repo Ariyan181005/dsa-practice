073f25929554eda55bccaed6ce32d415cfe9344d
Ariyan181005 <139477779+Ariyan181005@users.noreply.github.com>
2026-08-31 21:16:43 +0530

    Time: 1570 ms (92.11%), Space: 19.9 MB (99.37%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -4,6 +4,7 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        """
         if n <= 2:
             return 0
         p = [True] * n
@@ -12,3 +13,14 @@ class Solution(object):
             if p[i]:
                 p[i * i:n:i] = [False] * (((n - 1 - i * i) // i) + 1)
         return sum(p)
+        """
+        if n<=2:
+            return 0
+        p = bytearray([1])*n
+        p[0] = p[1] = 0
+        for i in range(2, int(n**0.5)+1):
+            if p[i]:
+                r = (n-1-i*i) // i+1
+                p[i*i:n:i] = bytearray([0])*r
+        return sum(p)
+        
