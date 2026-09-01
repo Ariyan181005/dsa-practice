f8bd97a85e039bfb79a55ec01edf4d9e810d2757
Ariyan181005 <139477779+Ariyan181005@users.noreply.github.com>
2026-09-01 22:41:42 +0530

    Time: 15 ms (89.07%), Space: 25.8 MB (92.19%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,9 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
+        """
+        # normal way
         s=set()
         for i in nums:
             if i in s:
                 return True
             s.add(i)
         return False
+        """
+        #easiest way
+        return len(nums)!=len(set(nums))
