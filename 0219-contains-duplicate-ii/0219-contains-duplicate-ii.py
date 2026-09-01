56817ed1c8730481def8f34ba272e69954be6372
Ariyan181005 <139477779+Ariyan181005@users.noreply.github.com>
2026-09-01 22:49:15 +0530

    Time: 36 ms (87.06%), Space: 29 MB (16.5%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -5,11 +5,18 @@ class Solution(object):
         :type k: int
         :rtype: bool
         """
-        
+        """
         a={}
         for i in range(len(nums)):
             if nums[i] in a :
                 if i - a[nums[i]] <= k:
                     return True
             a[nums[i]] = i
+        return False
+        """
+        pos = {}
+        for i,x in enumerate(nums):
+            if x in pos and i - pos[x] <= k:
+                return True
+            pos[x] = i
         return False
