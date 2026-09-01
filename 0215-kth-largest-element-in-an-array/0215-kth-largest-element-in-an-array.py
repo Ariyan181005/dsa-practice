3dec010fef62cd9488048d058634938903ba544e
Ariyan181005 <139477779+Ariyan181005@users.noreply.github.com>
2026-09-01 22:28:57 +0530

    Time: 21 ms (99.81%), Space: 43.3 MB (5.23%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,4 +1,10 @@
+import numpy as np
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        """
+        #with sort
         nums.sort(reverse=True)
         return nums[k-1]
+        """
+        #without sort
+        return int(np.partition(nums, -k)[-k])
