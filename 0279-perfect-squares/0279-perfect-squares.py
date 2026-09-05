1135ccdaa3537b9b31f6e9681de6e63e16cbdb04
Ariyan181005 <139477779+Ariyan181005@users.noreply.github.com>
2026-09-05 19:46:52 +0530

    Time: 2 ms (97.24%), Space: 12.4 MB (79.07%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -4,6 +4,7 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        """
         dp = [float('inf')] * (n + 1)
         dp[0] = 0
         for i in range(1, n + 1):
@@ -12,3 +13,12 @@ class Solution(object):
                 dp[i] = min(dp[i], dp[i - j * j] + 1)
                 j += 1
         return dp[n]
+        """
+        if int(sqrt(n))**2 == n: return 1
+        for j in range(int(sqrt(n)) + 1):
+            if int(sqrt(n - j*j))**2 == n - j*j: return 2
+            
+        while n % 4 == 0: 
+            n >>= 2
+        if n % 8 == 7: return 4
+        return 3
