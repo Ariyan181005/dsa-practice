ccfe557a7d723e3eec03e34f45ed7ad18ff9b96c
Ariyan181005 <139477779+Ariyan181005@users.noreply.github.com>
2026-09-06 17:32:15 +0530

    Time: 273 ms (96.63%), Space: 12.5 MB (57.22%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -5,15 +5,10 @@ class Solution(object):
         :type t: str
         :rtype: int
         """
-        m = len(s)
-        n = len(t)
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
-        for i in range(m + 1):
-            dp[i][0] = 1
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                if s[i - 1] == t[j - 1]:
-                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
-                else:
-                    dp[i][j] = dp[i - 1][j]
-        return dp[m][n]
+        dp = [0] * (len(t) + 1)
+        dp[0] = 1
+        for ch in s:
+            for j in range(len(t) - 1, -1, -1):
+                if ch == t[j]:
+                    dp[j + 1] += dp[j]
+        return dp[-1]
