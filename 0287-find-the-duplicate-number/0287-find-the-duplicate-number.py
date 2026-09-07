9c32c5e67450623f82f640c7dd14c352c3cee3f1
Ariyan181005 <139477779+Ariyan181005@users.noreply.github.com>
2026-09-07 20:57:19 +0530

    Time: 27 ms (87.57%), Space: 27.1 MB (15.81%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -4,20 +4,6 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        """
-        s = nums[0]
-        f = nums[0]
-        while True:
-            s = nums[s]
-            f = nums[nums[f]]
-            if s == f:
-                break
-        s = nums[0]
-        while s != f:
-            s = nums[s]
-            f = nums[f]
-        return s
-        """
         n=len(nums)
         s=set()
         for x in nums:
