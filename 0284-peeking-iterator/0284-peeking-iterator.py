6609fba998df3890b6fb69c295115cfab639632a
Ariyan181005 <139477779+Ariyan181005@users.noreply.github.com>
2026-09-07 20:51:15 +0530

    Time: 18 ms (49.84%), Space: 12.6 MB (45.34%) - LeetHub

--- 0284-peeking-iterator/0284-peeking-iterator.py
+++ 0284-peeking-iterator/0284-peeking-iterator.py
@@ -1,48 +1,16 @@
-# Below is the interface for Iterator, which is already defined for you.
-#
-# class Iterator(object):
-#     def __init__(self, nums):
-#         """
-#         Initializes an iterator object to the beginning of a list.
-#         :type nums: List[int]
-#         """
-#
-#     def hasNext(self):
-#         """
-#         Returns true if the iteration has more elements.
-#         :rtype: bool
-#         """
-#
-#     def next(self):
-#         """
-#         Returns the next element in the iteration.
-#         :rtype: int
-#         """
-
 class PeekingIterator(object):
     def __init__(self, iterator):
-        """
-        Initialize your data structure here.
-        :type iterator: Iterator
-        """
         self.iterator = iterator
         self.peeked = None
         self.has_peeked = False
 
     def peek(self):
-        """
-        Returns the next element in the iteration without advancing the iterator.
-        :rtype: int
-        """
         if not self.has_peeked:
             self.peeked = self.iterator.next()
             self.has_peeked = True
         return self.peeked
 
     def next(self):
-        """
-        :rtype: int
-        """
         if self.has_peeked:
             value = self.peeked
             self.peeked = None
@@ -51,13 +19,4 @@ class PeekingIterator(object):
         return self.iterator.next()
 
     def hasNext(self):
-        """
-        :rtype: bool
-        """
         return self.has_peeked or self.iterator.hasNext()
-
-# Your PeekingIterator object will be instantiated and called as such:
-# iter = PeekingIterator(Iterator(nums))
-# while iter.hasNext():
-#     val = iter.peek()   # Get the next element but not advance the iterator.
-#     iter.next()         # Should return the same value as [val].
