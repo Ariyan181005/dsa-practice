d862fa2f3adfe70888274ab3ab8b1d03696776f7
Ariyan181005 <139477779+Ariyan181005@users.noreply.github.com>
2026-09-10 21:17:26 +0530

    Time: 25 ms (46.83%), Space: 24.5 MB (28.07%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -4,14 +4,6 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        """
-        for i in range (len(nums)):
-            if nums[i]<nums[i+1]<nums[i+2]:
-                break
-            else:
-                return False
-        return True
-        """
         a=float('inf')
         b=float('inf')
         for i in nums:
