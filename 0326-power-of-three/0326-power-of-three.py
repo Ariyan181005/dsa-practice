094b79821e484d79f338708738fa9edcda8c5608
Ariyan181005 <139477779+Ariyan181005@users.noreply.github.com>
2026-09-10 20:53:46 +0530

    Time: 9 ms (68.49%), Space: 12.3 MB (90.57%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.py
+++ 0326-power-of-three/0326-power-of-three.py
@@ -4,6 +4,8 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
+        """
+        #easy but time consuming
         if n <= 0:
             return False
         while n>1:
@@ -12,3 +14,10 @@ class Solution(object):
             else:
                 return False
         return True
+        """
+        #recursive
+        if n == 1:
+            return True
+        if n <= 0 or n % 3 != 0:
+            return False
+        return self.isPowerOfThree(n // 3)
