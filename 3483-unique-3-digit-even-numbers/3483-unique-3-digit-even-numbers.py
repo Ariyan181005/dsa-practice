0ea7f2ff1c1adc3b232583469e0aac24c61d53f4
Ariyan181005 <139477779+Ariyan181005@users.noreply.github.com>
2026-09-11 22:54:06 +0530

    Time: 17 ms (86.51%), Space: 12.6 MB (6.35%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
@@ -4,21 +4,8 @@ class Solution(object):
         :type digits: List[int]
         :rtype: int
         """
-        ans = set()
-        for i in range(1, 10):
-            for j in range(10):
-                for k in range(0, 10, 2):
-                    if i in digits and j in digits and k in digits:
-                        a = digits[:]
-                        a.remove(i)
-                        if j in a:
-                            a.remove(j)
-                        else:
-                            continue
-                        if k in a:
-                            a.remove(k)
-                        else:
-                            continue
-                        num = i * 100 + j * 10 + k
-                        ans.add(num)
-        return len(ans)
+        numbers = set()
+        for a,b,c in permutations(digits,3):
+            if a != 0 and c%2 == 0:
+                numbers.add((a,b,c))
+        return len(numbers)
