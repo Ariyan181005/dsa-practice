3c505132c13ff8383f75daf0ae7daa82ec08d704
Ariyan181005 <139477779+Ariyan181005@users.noreply.github.com>
2026-09-17 15:11:19 +0530

    Time: 97 ms (89.39%), Space: 31.1 MB (66.48%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.py
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.py
@@ -1,21 +1,15 @@
 class Solution:
     def minSumOfLengths(self, arr: List[int], target: int) -> int:
-        n = len(arr)
-        mp = {0: -1}
-        b = [n + 1] * n
-        s = 0
-        ans = n + 1
-        mn = n + 1
-        for i in range(n):
-            s += arr[i]
-            if s - target in mp:
-                j = mp[s - target]
-                ln = i - j
-                if j >= 0:
-                    ans = min(ans, ln + b[j])
-                mn = min(mn, ln)
-            b[i] = mn
-            mp[s] = i
-        if ans == n + 1:
-            return -1
-        return ans
+        n, ans, tot = len(arr), len(arr) + 1, 0
+        dp = [n] * (n + 1)
+        lt = 0
+        for rt in range(len(arr)):
+            tot+=arr[rt]
+            while tot>target:
+                tot-=arr[lt]
+                lt+=1
+            dp[rt+1]=dp[rt]
+            if tot==target:
+                ans=min(ans,rt-lt+1+dp[lt])
+                dp[rt+1]=min(dp[rt],rt-lt+1)
+        return -1 if ans==n+1 else ans
