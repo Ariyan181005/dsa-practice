da2c6a56613484ca36e90bf6f9ec8ea4581c922b
Ariyan181005 <139477779+Ariyan181005@users.noreply.github.com>
2026-09-20 16:23:21 +0530

    Time: 55 ms (5.27%), Space: 22.7 MB (94.34%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,19 @@
+class Solution:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        ans=[]
+        dic={}
+        for i in nums:
+            if i not in dic.keys():
+                dic[i]=1
+            else:
+                dic[i]+=1
+        for i in range(k):
+            mx = 0
+            ele = 0
+            for j in dic:
+                if dic[j] > mx:
+                    mx = dic[j]
+                    ele = j
+            ans.append(ele)
+            del dic[ele]
+        return ans
