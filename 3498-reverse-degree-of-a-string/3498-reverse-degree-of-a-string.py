ae9fd3c63dbc7f3dea92c793fbb35859a67e0cfa
Ariyan181005 <139477779+Ariyan181005@users.noreply.github.com>
2026-09-20 05:57:33 +0530

    Time: 7 ms (70%), Space: 19.4 MB (19.13%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
@@ -2,6 +2,5 @@ class Solution:
     def reverseDegree(self, s: str) -> int:
         ans=0
         for i in range (len(s)):
-            revind = 26 - (ord(s[i]) - ord('a'))
-            ans+=revind*(i+1)
+            ans += (26-(ord(s[i])-ord('a')))*(i+1)
         return ans
